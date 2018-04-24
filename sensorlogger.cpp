aa997f139563f26da0048dc0dfbf9ac6c8c0ba64
Przemek Recha <przemek.recha@gmail.com>
2018-04-24 15:19:51 +0200

    Klasa logowania

--- sensorlogger.cpp
+++ sensorlogger.cpp
@@ -1,6 +1,35 @@
 #include "sensorlogger.h"
+#include "sensordevice.h"
 
-SensorLogger::SensorLogger(QObject *parent) : QObject(parent)
-{
+SensorLogger::SensorLogger(QObject *parent) : QObject(parent) {
 
 }
+
+SensorLogger::~SensorLogger() {
+
+}
+
+void SensorLogger::openLog(QString fileName) {
+    logFile.setFileName(fileName);
+    if (logFile.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+        logStream.setDevice(&logFile);
+    }
+}
+
+void SensorLogger::writeToLog(QList<SensorDevice *> sensorList) {
+    if(logFile.isOpen()) {
+        for (int i = 0; i < 4; i++) {
+            for(QList<SensorDevice *>::iterator it = sensorList.begin(); it != sensorList.end(); it++) {
+                logStream << (*it)->getSensorValue()[i] << " ";
+            }
+            logStream << "; ";
+        }
+        logStream << endl;
+    }
+}
+
+void SensorLogger::writeToLogSlot(QList<SensorDevice *> sensorList) {
+    writeToLog(sensorList);
+}
+
+
--- sensorlogger.h
+++ sensorlogger.h
@@ -2,17 +2,29 @@
 #define SENSORLOGGER_H
 
 #include <QObject>
+#include <QFile>
+#include <QTextStream>
+#include <QList>
+
+class SensorDevice;
 
 class SensorLogger : public QObject {
     Q_OBJECT
 private:
+    QFile logFile;
+    QTextStream logStream;
 
 public:
     explicit SensorLogger(QObject *parent = nullptr);
+    ~SensorLogger();
+
+    void openLog(QString fileName);
+    void writeToLog(QList<SensorDevice*> sensorList);
 
 signals:
 
 public slots:
+    void writeToLogSlot(QList<SensorDevice*> sensorList);
 };
 
 #endif // SENSORLOGGER_H
